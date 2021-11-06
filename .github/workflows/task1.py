59c10260ad5ce654454e0398c3b7de4436568b9e
Mpisano10 <91083469+Mpisano10@users.noreply.github.com>
2021-11-06 16:34:54 +0100

    Update task1.py

--- .github/workflows/task1.py
+++ .github/workflows/task1.py
@@ -6,3 +6,7 @@ import json
 #API request
 JSONContent = requests.get("https://api.magicthegathering.io/v1/cards").json()
 
+#Json normalization
+df =pd.DataFrame(JSONContent["cards"])
+from pandas.io.json import json_normalize
+pd.json_normalize(JSONContent["cards"])
