1b2225105db8d3c6f7a6c538225d50a526cb990c
simonelugithub <93279046+simonelugithub@users.noreply.github.com>
2021-11-06 16:35:14 +0100

    Update task1.py

--- .github/workflows/task1.py
+++ .github/workflows/task1.py
@@ -10,3 +10,10 @@ JSONContent = requests.get("https://api.magicthegathering.io/v1/cards").json()
 df =pd.DataFrame(JSONContent["cards"])
 from pandas.io.json import json_normalize
 pd.json_normalize(JSONContent["cards"])
+
+#clean usefull columns
+df = df.drop(columns = df.columns[[4,6,7,9,13,16,18,19,20,21,28,27,26,25,23,22]])
+df = df.fillna(0)
+
+#write csv file
+df.to_csv(r"C:\Users\loren\OneDrive\Desktop\Pythonproject_Querini\MGT.csv")
